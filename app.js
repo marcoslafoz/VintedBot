7ece59ec2dd4533edd4229aa1acbba87df3e9e73
Marcos Lafoz <marcos.lafoz96@gmail.com>
2023-09-23 21:10:03 +0200

    fix views generator

--- app.js
+++ app.js
@@ -13,38 +13,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
-app.post('/procesar_link', (req, res) => {
-    const vintedLink = req.body.vinted_link;
-
-    const scriptPath = path.join(__dirname, 'static', 'scripts');
-
-    const options = {
-        scriptPath: scriptPath,
-    };
-
-    // Crear una nueva instancia de PythonShell para cada solicitud
-    const pyShell = new PythonShell('procesar_enlace.py', options);
-
-    pyShell.send({ vintedLink }); // Envía los datos al script Python
-
-    pyShell.on('message', (message) => {
-        try {
-            const resultadoPython = JSON.parse(message);
-            res.send(resultadoPython); // Envía la respuesta al cliente
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Error al procesar el enlace.');
-        }
-    });
-
-    pyShell.end((err) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error al procesar el enlace.');
-        }
-    });
-});
-
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,22 +4,72 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
         const vintedLink = document.getElementById("vinted_link_input").value;
         if (vintedLink !== "") {
-            fetch("/procesar_link", {
-                method: "POST",
-                body: JSON.stringify({ vintedLink }),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-                .then(response => response.text())
-                .then(resultado => {
-                    console.log(resultado); // Aquí obtienes la respuesta del servidor
-                })
-                .catch(error => {
-                    console.error("Error al enviar la solicitud al servidor:", error);
-                });
+            console.log("Añadiendo visitas")
+            generator(vintedLink)
+            alert("Añadiendo visitas");
         } else {
-            console.log("Formulario vacío");
+            console.log("Formulario vacío")
         }
     });
 });
+
+function generator(vinted_link) {
+    const threadsInput = '1'; // Establece el valor de threadsInput en 1
+
+    if (!vinted_link.includes('vinted')) {
+        console.log('Invalid vinted link');
+        return;
+    }
+
+    const threads = parseInt(threadsInput);
+
+    if (isNaN(threads)) {
+        console.log('Invalid threads number');
+        return;
+    }
+
+    let ctr = 0;
+
+    const safePrint = (thread_id, arg) => {
+        console.log(`[Thread ${thread_id}] ${arg}`);
+    };
+
+    const main = async (thread_id) => {
+        const headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+        };
+
+        while (true) {
+            try {
+                const response = await fetch(vinted_link, { headers });
+
+                if (response.status === 200) {
+                    ctr++;
+                    safePrint(thread_id, `Added 1 view to product ${vinted_link}. Total views added: ${ctr}`);
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                } else if (response.status === 429) {
+                    safePrint(thread_id, `Ip address rate limited ${vinted_link}`);
+                    await new Promise(resolve => setTimeout(resolve, 5000));
+                } else {
+                    safePrint(thread_id, `Error ${response.status}`);
+                    await new Promise(resolve => setTimeout(resolve, 3000));
+                }
+            } catch (e) {
+                safePrint(thread_id, `Request failed, retrying ${e.message}`);
+                await new Promise(resolve => setTimeout(resolve, 2000));
+            }
+        }
+    };
+
+    const runThreads = () => {
+        for (let i = 0; i < threads; i++) {
+            try {
+                main(i);
+            } catch (e) {
+                setTimeout(() => { }, 1000);
+            }
+        }
+    };
+
+    runThreads();
+}
