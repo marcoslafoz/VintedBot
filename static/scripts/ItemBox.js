1779d18bf85c1faa4ee89dacfbfb530c9f6b482c
marcoslafoz <marcos.lafoz96@gmail.com>
2023-10-16 17:27:00 +0200

    indent size

--- static/scripts/ItemBox.js
+++ static/scripts/ItemBox.js
@@ -1,31 +1,29 @@
 function showViews(views) {
-
-    let realViews = views+1
-    const viewsBox = document.getElementById("viewsText")
-    viewsBox.textContent = ("+" + realViews + " 👁️")
+  let realViews = views + 1;
+  const viewsBox = document.getElementById("viewsText");
+  viewsBox.textContent = "+" + realViews + " 👁️";
 }
 
 function loadItemBox(vintedURL) {
+  const divPrincipal = document.createElement("div");
+  divPrincipal.className = "item-container";
 
-    const divPrincipal = document.createElement("div")
-    divPrincipal.className = "item-container"
+  const divUrl = document.createElement("div");
+  divUrl.className = "item-url";
+  const parrafoUrl = document.createElement("p");
+  parrafoUrl.textContent = vintedURL;
+  divUrl.appendChild(parrafoUrl);
 
-    const divUrl = document.createElement("div")
-    divUrl.className = "item-url"
-    const parrafoUrl = document.createElement("p")
-    parrafoUrl.textContent = vintedURL
-    divUrl.appendChild(parrafoUrl)
+  const divViews = document.createElement("div");
+  divViews.className = "item-views";
+  const parrafoViews = document.createElement("p");
+  parrafoViews.id = "viewsText";
 
-    const divViews = document.createElement("div")
-    divViews.className = "item-views"
-    const parrafoViews = document.createElement("p")
-    parrafoViews.id = "viewsText"
+  parrafoViews.textContent = "+0 👁️";
+  divViews.appendChild(parrafoViews);
 
-    parrafoViews.textContent = "+0 👁️"
-    divViews.appendChild(parrafoViews)
+  divPrincipal.appendChild(divUrl);
+  divPrincipal.appendChild(divViews);
 
-    divPrincipal.appendChild(divUrl)
-    divPrincipal.appendChild(divViews)
-
-    document.body.appendChild(divPrincipal)
+  document.body.appendChild(divPrincipal);
 }
--- static/scripts/ViewsGenerator.js
+++ static/scripts/ViewsGenerator.js
@@ -1,45 +1,45 @@
 async function generateVisits(vintedLink, views) {
-    const threads = 1
-    let counter = 0
+  const threads = 1;
+  let counter = 0;
 
-    const main = async () => {
-        const headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
+  const main = async () => {
+    const headers = {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    };
+
+    loadItemBox(vintedLink);
+
+    for (let i = 0; i < views; i++) {
+      try {
+        const response = await fetch(vintedLink, { headers });
+
+        if (response.status === 200) {
+          counter++;
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        } else if (response.status === 429) {
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+        } else {
+          await new Promise((resolve) => setTimeout(resolve, 3000));
         }
-
-        loadItemBox(vintedLink)
-
-        for (let i = 0; i < views; i++) {
-            try {
-                const response = await fetch(vintedLink, { headers })
-
-                if (response.status === 200) {
-                    counter++
-                    await new Promise(resolve => setTimeout(resolve, 1000));
-                    
-                } else if (response.status === 429) {
-                    await new Promise(resolve => setTimeout(resolve, 5000))
-                } else {
-                    await new Promise(resolve => setTimeout(resolve, 3000))
-                }
-            } catch (e) {
-                await new Promise(resolve => setTimeout(resolve, 2000))
-            }
-            showViews(i)
-        }
-
-        alert("Added " + views + " visits")
+      } catch (e) {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      }
+      showViews(i);
     }
 
-    const runThreads = () => {
-        for (let i = 0; i < threads; i++) {
-            try {
-                main(i);
-            } catch (e) {
-                setTimeout(() => { }, 1000)
-            }
-        }
-    }
+    alert("Added " + views + " visits");
+  };
 
-    runThreads()
+  const runThreads = () => {
+    for (let i = 0; i < threads; i++) {
+      try {
+        main(i);
+      } catch (e) {
+        setTimeout(() => {}, 1000);
+      }
+    }
+  };
+
+  runThreads();
 }
